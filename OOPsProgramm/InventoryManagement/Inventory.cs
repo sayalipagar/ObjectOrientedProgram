c72279ad9e432d03bf80f8d397fbf3057f25dcb9
sayalipagar <pagarsayali565@gmail.com>
2022-09-10 15:05:43 +0530

    [sayali]Add:InventoryJson

---
+++ OOPsProgramm/InventoryManagement/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsProgramm.InventoryManagement
+{
+    internal class Inventory
+    {
+        List<InventoryDetails> details = new List<InventoryDetails>();
+
+
+        public void ReadJsonFile(string fileName)
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                var json = reader.ReadToEnd();
+                this.details = JsonConvert.DeserializeObject<List<InventoryDetails>>(json);
+                Console.WriteLine("Name" + "\t" + "Weight" + "\t" + "Price" + "\t" + "PricePerKg");
+                foreach (var data in details)
+                {
+                    Console.WriteLine(data.Name + "\t" + data.Weight + "\t" + data.Price + "\t" + data.Weight + data.Price);
+                }
+            }
+        }
+    }
+}
--- OOPsProgramm/Program.cs
+++ OOPsProgramm/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace OOPsProgramm
+﻿using OOPsProgramm.InventoryManagement;
+
+namespace OOPsProgramm
 {
     internal class Program
     {
+        const string INVENTORY_DATA_FILE_PATH = @"D:\DotNetPrograms\ObjectOrientedProgram\OOPsProgramm\InventoryManagement\Inventory.json";
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Object Oriented Programms!!!");
+            while (true)
+            {
+                Console.WriteLine("\nSelect Programm\n 1.Json Inventory Data Mangement");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Inventory inventory = new Inventory();
+                        inventory.ReadJsonFile(INVENTORY_DATA_FILE_PATH);
+                        break;
+                }
+            }
         }
     }
 }
